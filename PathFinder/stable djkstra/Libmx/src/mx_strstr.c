27adeb242094c2bbbb225654f9d5f49d11fc38a1
ronald112 <ronald112@.github.com>
2019-12-20 01:30:19 +0200

    20.12.19

---
+++ PathFinder/stable djkstra/Libmx/src/mx_strstr.c
@@ -0,0 +1,17 @@
+#include "libmx.h"
+char *mx_strstr(const char *haystack, const char *needle) {
+    if (needle == NULL || haystack == NULL)
+        return NULL;
+    int i = 0;
+    for (; *haystack != '\0'; i++, haystack++) {
+        if (*haystack == *needle) {            
+            for (int j = i; *needle != '\0'; j++, needle++) {
+                if (haystack[j] != *needle) {
+                    break;
+                }
+            }
+            return (char*)haystack;
+        }        
+    }
+    return (char*)needle;
+}
