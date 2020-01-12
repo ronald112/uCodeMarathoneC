16f5c8570ef2d64edc612177c83fd2a867135445
ronald112 <ronald112@.github.com>
2020-01-13 01:42:54 +0200

    fix

--- PathFinder/djikstra-test-listOutput/yburienkov/src/mx_dijkstra.c
+++ PathFinder/djikstra-test-listOutput/yburienkov/src/mx_dijkstra.c
@@ -7,7 +7,7 @@ int *mx_return_int(int num) {
     return ret;
 }
 
-t_list *mx_get_element_by_index(t_list *list, int index) {
+t_list *mx_lget_lat(t_list *list, int index) {
     t_list *temp = list;
 
     for (; temp && index--; temp = temp->next);
@@ -29,7 +29,7 @@ static void add_route_to_list_node(t_fin_out *fin_out, t_list *route) {
 
     for (int i = 0; i < size; ++i) {
         temp = fin_out->l_parent[*(int *)route->data];
-        temp = mx_get_element_by_index(temp, i);
+        temp = mx_lget_lat(temp, i);
 
         mx_push_front(&route, mx_return_int(*(int *)temp->data));
 
@@ -57,15 +57,15 @@ static void mx_print_route(t_fin_out *fin_out, t_grph *graph, t_list *route) {
     int list_size = mx_list_size(route);
 
     for (int i = 0; i < list_size - 1 && fin_out; i++) {
-        temp = mx_get_element_by_index(route, i);
+        temp = mx_lget_lat(route, i);
         mx_printstr(graph->isld[*(int *)temp->data]);
         mx_printstr(" -> ");
     }
-    temp = mx_get_element_by_index(route, list_size - 1);
+    temp = mx_lget_lat(route, list_size - 1);
     mx_printstr(graph->isld[*(int *)temp->data]);
 }
 
-void *mx_get_last_list_el(t_list *src) {
+void *mx_lget_back_el(t_list *src) {
     t_list *temp = src;
 
     for (; temp->next; temp = temp->next);
@@ -78,15 +78,14 @@ static void mx_print_dst(t_fin_out *fin_out, t_list *route) {
     int dist = 0;
 
     for (int i = 1; i < list_size - 1; i++) {
-        temp = mx_get_element_by_index(route, i);
+        temp = mx_lget_lat(route, i);
         mx_printint(fin_out->isld_weight[*(int *)temp->data] - dist);
         mx_printstr(" + ");
         dist = fin_out->isld_weight[*(int *)temp->data];
     }
-    mx_printint(fin_out->isld_weight[*(int *)mx_get_last_list_el(route)] 
-                                                                - dist);
+    mx_printint(fin_out->isld_weight[*(int *)mx_lget_back_el(route)] - dist);
     mx_printstr(" = ");
-    mx_printint(fin_out->isld_weight[*(int *)mx_get_last_list_el(route)]);
+    mx_printint(fin_out->isld_weight[*(int *)mx_lget_back_el(route)]);
 }
 
 static void mx_print_routes(t_fin_out *fin_out, t_grph *graph) {
@@ -95,7 +94,7 @@ static void mx_print_routes(t_fin_out *fin_out, t_grph *graph) {
         mx_printstr("Path: ");
         mx_printstr(graph->isld[fin_out->src]);
         mx_printstr(" -> ");
-        mx_printstr(graph->isld[*(int *)mx_get_last_list_el(temp->data)]);
+        mx_printstr(graph->isld[*(int *)mx_lget_back_el(temp->data)]);
         mx_printstr("\nRoute: ");
         mx_print_route(fin_out, graph, temp->data);
         mx_printstr("\nDistance: ");
@@ -103,7 +102,7 @@ static void mx_print_routes(t_fin_out *fin_out, t_grph *graph) {
             mx_print_dst(fin_out, temp->data);
         else
             mx_printint(fin_out->isld_weight[
-                *(int *)mx_get_last_list_el(temp->data)]);
+                *(int *)mx_lget_back_el(temp->data)]);
         mx_printstr("\n========================================\n");
     }
 }
